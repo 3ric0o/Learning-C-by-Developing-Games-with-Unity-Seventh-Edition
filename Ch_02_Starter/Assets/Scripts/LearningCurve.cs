975d9f47abcace1ce482dcb81e6e6989935f3b00
3ric0o <3ric0o99@gmail.com>
2024-10-03 14:03:54 +0200

    Finished Chapter 2

--- Ch_02_Starter/Assets/Scripts/LearningCurve.cs
+++ Ch_02_Starter/Assets/Scripts/LearningCurve.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
-{
+{ 
+    [SerializeField] private int currentAge;
+    [SerializeField] private int addedAge;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        ComputeAge();
     }
 
+    /// <summary>
+    /// Computes a modified age by adding two variables together
+    /// </summary>
+    private void ComputeAge()
+    {
+        Debug.Log(currentAge + addedAge);
+    }
+    
     // Update is called once per frame
     void Update()
     {
