5289a0005f6813ff08825ac0358f8d1e314ccaa7
3ric0o <3ric0o99@gmail.com>
2024-10-03 16:28:08 +0200

    Ended on Site 58

--- Ch_03_Starter/Assets/Scripts/LearningCurve.cs
+++ Ch_03_Starter/Assets/Scripts/LearningCurve.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LearningCurve : MonoBehaviour
+//IM ON SITE 58 IN THE BOOK
 {
-    public int CurrentAge = 30;
+    private int CurrentAge = 30;
     public int AddedAge = 1;
+
+    public float pi = 3.14f;
+    public string firstName = "Harrison";
+    public bool isAuthor = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(30 + 1);
-        Debug.Log(CurrentAge + 1);
-
         ComputeAge();
+        Debug.Log($"A string can have variables like {firstName} inserted directly!");
     }
 
     /// <summary>
